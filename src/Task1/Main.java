27ee33d6effca0909be4cd953cfdcbb9efbe4da6
anclko <ackoch@hotmail.com>
2023-12-20 16:31:26 +0100

    renamed variables for clearness add task 1B

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -2,14 +2,21 @@ package Task1;
 
 public class Main {
     public static void main(String[] args) {
-        // Task 1A: Present the result of inserting elements into a binary heap
+        int[] array = {10, 12, 1, 14, 6, 5, 8, 15, 3, 9, 7, 4, 11, 13, 2};
+
+        // ----------------------- Task 1A --------------------------- //
         Task1A.BinaryHeap heap1A = new Task1A.BinaryHeap(15);
-        int[] elements1A = {10, 12, 1, 14, 6, 5, 8, 15, 3, 9, 7, 4, 11, 13, 2};
 
         System.out.println("Task 1A: Inserting elements into BinaryHeap");
-        for (int element : elements1A) {
-            heap1A.insert(element);
+        for (int numbers : array) {
+            heap1A.insert(numbers);
             heap1A.printHeap();
         }
+
+        // ----------------------- Task 1B --------------------------- //
+        Task1B.linearHeap linearHeap = new Task1B.linearHeap(array);
+        System.out.println(" ");
+        System.out.println("Task 1B: Result using Linear-Time Algorithm:");
+        linearHeap.printHeap();
     }
 }
