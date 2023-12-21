86d62424019b37968f13e515c28b26901ecb516c
anclko <ackoch@hotmail.com>
2023-12-21 12:41:14 +0100

    calling level order method

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -5,6 +5,7 @@ public class Main {
         int[] array = {10, 12, 1, 14, 6, 5, 8, 15, 3, 9, 7, 4, 11, 13, 2};
 
         // ----------------------- Task 1A + 1C --------------------------- //
+        System.out.println("\n---------------------------- Algorithm 1 ---------------------------------");
         Task1A.BinaryHeap heap1A = new Task1A.BinaryHeap(15);
 
         System.out.println("Task 1A: Inserting elements");
@@ -21,13 +22,23 @@ public class Main {
         System.out.println("\nPost-Order Traversal:");
         heap1A.postOrder(0);
         System.out.println("\nLevel-Order Traversal:");
-
-
+        heap1A.levelOrder();
+        System.out.println("");
 
         System.out.println("\n---------------------------- Algorithm 2 ---------------------------------");
         // ----------------------- Task 1B + 1C --------------------------- //
         Task1B.linearHeap linearHeap = new Task1B.linearHeap(array);
-        System.out.println("Task 1B: Linear Time Algorithm:");
+        System.out.println("\nTask 1B: Linear Time Algorithm:");
         linearHeap.printHeap();
+
+        System.out.println("\nTask 1C: Traversals For Algorithm 2");
+        System.out.println("In-Order Traversal:");
+        heap1A.inOrder(0);
+        System.out.println("\nPre-Order Traversal:");
+        heap1A.preOrder(0);
+        System.out.println("\nPost-Order Traversal:");
+        heap1A.postOrder(0);
+        System.out.println("\nLevel-Order Traversal:");
+        heap1A.levelOrder();
     }
 }
