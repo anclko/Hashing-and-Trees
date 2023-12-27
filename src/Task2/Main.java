45ab139af17c9b1be06c1f69e66a0412fbe1d79e
anclko <ackoch@hotmail.com>
2023-12-27 14:17:54 +0100

    called method in main

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -2,8 +2,25 @@ package Task2;
 
 public class Main {
     public static void main(String[] args) {
+        separateChaining separateChainingTable = new separateChaining(10);
+        linearProbing linearProbing = new linearProbing(10);
 
+        int[] data = {4371, 1323, 6173, 4199, 4344, 9679, 1989};
 
+        //Seperate Chaining
+        System.out.println("Separate Chaining:");
+        for (int item : data) {
+            separateChainingTable.separateChainingInsert(item);
+        }
+        separateChainingTable.display();
+        System.out.println();
 
+        //Linear Probing
+        System.out.println("Linear Probing:");
+        for (int item : data) {
+            linearProbing.linearProbingInsert(item);
+        }
+        // Display the hash table
+        linearProbing.display();
     }
 }
